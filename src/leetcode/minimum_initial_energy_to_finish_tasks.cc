218b61e0c24dc6960e594dc8326c6a4e65e9f5e3
nnovikov <nicknovikov10@gmail.com>
2022-10-25 15:30:07 +0300

    add minimum_initial_energy_to_finish_tasks solution with usage of standart library

--- src/leetcode/minimum_initial_energy_to_finish_tasks.cc
+++ src/leetcode/minimum_initial_energy_to_finish_tasks.cc
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <format>
 #include <iostream>
+#include <numeric>
 #include <unordered_map>
 #include <vector>
 
@@ -13,23 +14,35 @@ using namespace std;
 
 class Solution {
  public:
-  int minimumEffort(vector<vector<int>>& tasks) {
+  /*int minimumEffort(vector<vector<int>>& tasks) {
     std::sort(tasks.begin(), tasks.end(),
               [](const std::vector<int>& a1, const std::vector<int>& a2) {
                 auto d1 = a1[1] - a1[0];
                 auto d2 = a2[1] - a2[0];
-                // return d1 == d2 ? d1 > d2 : a1[0] < a2[0];
-                return d1 < d2;
+                if (d1 < d2) return true;
+                if (d1 == d2) return a1[0] > a2[0];
+                return false;
               });
     int res = 0;
     for (auto task : tasks) {
       res = std::max(res + task[0], task[1]);
-      /*std::cout << std::format(
-                       "delay {:2d}, actual {:2d}, minimum {:2d}. res = {:2d}",
-                       task[1] - task[0], task[0], task[1], res)
-                << std::endl;*/
+      std::cout << std::format(
+          "delay {:2d}, actual {:2d}, minimum {:2d}. res = {:2d}\n",
+          task[1] - task[0], task[0], task[1], res);
     }
     return res;
+  }*/
+
+  int minimumEffort(vector<vector<int>>& tasks) {
+    auto diffComparator = [](const std::vector<int>& a1,
+                             const std::vector<int>& a2) {
+      return (a1[1] - a1[0]) < (a2[1] - a2[0]);
+    };
+    auto reducer = [](int res, const std::vector<int>& task) {
+      return std::move(std::max(res + task[0], task[1]));
+    };
+    std::sort(tasks.begin(), tasks.end(), diffComparator);
+    return std::accumulate(tasks.begin(), tasks.end(), 0, reducer);
   }
 };
 }  // namespace leetcode
