8da3eab9025cb7494027d9de5dd292a86e937f10
nnovikov <nicknovikov10@gmail.com>
2022-10-31 17:02:46 +0300

    solve daily_temperatures with monotonic stack

--- src/leetcode/daily_temperatures.cc
+++ src/leetcode/daily_temperatures.cc
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <format>
 #include <iostream>
+#include <stack>
 #include <unordered_map>
 #include <vector>
 
@@ -12,17 +13,29 @@ using namespace std;
 
 class Solution {
  public:
+  Solution() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    wcin.tie(nullptr);
+    cerr.tie(nullptr);
+    wcerr.tie(nullptr);
+    clog.tie(nullptr);
+    wclog.tie(nullptr);
+  }
   vector<int> dailyTemperatures(vector<int>& temperatures) {
     int size = temperatures.size();
     std::vector<int> res(size, 0);
+    std::stack<int> stack;
     for (int i = 0; i < size; ++i) {
-      int j = i;
-      bool greater = temperatures[i] >= temperatures[j];
-      for (; j < size && greater; ++j)
-        greater = temperatures[i] >= temperatures[j];
-      res[i] = greater ? 0 : j - i - 1;
+      // std::cout << std::format("Checking {:2d}:", temperatures[i]);
+      while (!stack.empty() && temperatures[i] > temperatures[stack.top()]) {
+        // std::cout << std::format("-{}({})", temperatures[stack.top()], i - stack.top());
+        res[stack.top()] = i - stack.top();
+        stack.pop();
+      }
+      stack.push(i);
+      // std::cout << std::format("\n");
     }
-    // for (auto it : res) std::cout << std::format(" - {}\n", it);
     return res;
   }
 };
