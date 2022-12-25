7a47e702e816481b8ebcbc2968f724f10fdd4b93
nnovikov <nicknovikov10@gmail.com>
2022-12-25 07:46:13 +0300

    optimize longest_subsequence_with_limited_sum

--- src/leetcode/longest_subsequence_with_limited_sum.cc
+++ src/leetcode/longest_subsequence_with_limited_sum.cc
@@ -19,24 +19,26 @@ using namespace std;
 
 class Solution {
  public:
+  Solution() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    wcin.tie(nullptr);
+    cerr.tie(nullptr);
+    wcerr.tie(nullptr);
+    clog.tie(nullptr);
+    wclog.tie(nullptr);
+  }  
   vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
     int n = nums.size(), m = queries.size();
     std::vector<int> res(m);
-
-    for (int q = 0; q < m; ++q) {
-      std::priority_queue<int> queue;
-      for (int sum = 0, j = 0; j < n; ++j) {
-        if (nums[j] > queries[q]) continue;
-        queue.push(nums[j]);
-        sum += nums[j];
-        if (sum > queries[q]) {
-          sum -= queue.top();
-          queue.pop();
-        }
-        res[q] = std::max<int>(queue.size(), res[q]);
-      }
+    std::sort(nums.begin(), nums.end());
+    for (int q = 0, query = 0; q < m; ++q) {
+      query = queries[q];
+      queries[q] = 0;
+      for (int sum = 0, j = 0; j < n; ++j)
+        if ((sum += nums[j]) <= query) ++queries[q];
     }
-    return res;
+    return queries;
   }
 };
 }  // namespace leetcode
