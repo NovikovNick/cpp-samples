ef36ed06730d531d634b4e243316c3b0c5539632
nnovikov <nicknovikov10@gmail.com>
2023-06-24 07:13:18 +0700

    add methods to sample dll

--- dll_sample/sample_dll.cc
+++ dll_sample/sample_dll.cc
@@ -1,3 +1,21 @@
 #include "sample_dll.h"
 
+Platform evalOperation(const Location& location, Operation operation) {
+  Platform res = {};
+  switch (operation) {
+    case Operation::PLUS: {
+      for (int i = 0; i < location.platforms_count; ++i)
+        res.id += location.platforms[i].id;
+      break;
+    }
+
+    case Operation::MINUS: {
+      for (int i = 0; i < location.platforms_count; ++i)
+        res.id -= location.platforms[i].id;
+      break;
+    }
+  }
+  return res;
+}
+
 long long getMicrosecondsInOneTick() { return 42; }
--- dll_sample/sample_dll.h
+++ dll_sample/sample_dll.h
@@ -2,6 +2,23 @@
 #define SAMPLE_DLL_H
 #define EXPORT extern "C" __declspec(dllexport)
 
+enum Operation {
+  PLUS = 0,
+  MINUS = 1
+};
+
+struct Platform {
+  int id, type, width, height;
+};
+
+struct Location {
+  bool is_1st_player;
+  Platform* platforms;
+  unsigned long long platforms_count;
+};
+
+EXPORT Platform evalOperation(const Location& location, Operation operation);
+
 EXPORT long long getMicrosecondsInOneTick();
 
 #endif  // SAMPLE_DLL_H
