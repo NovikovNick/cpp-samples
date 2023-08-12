55153b726aa034bbb8f9e52208669e52a69c1dc3
nnovikov <nicknovikov10@gmail.com>
2023-08-12 19:44:34 +0700

    add fold expression sample

---
+++ src/cpp_features/fold.h
@@ -0,0 +1,44 @@
+#ifndef CPP_FEATURES_FOLD_H
+#define CPP_FEATURES_FOLD_H
+#include <vector>
+
+/// <summary>
+/// https://en.cppreference.com/w/cpp/language/fold
+/// </summary>
+namespace fold {
+
+void log(int x) { util::debug("{}\n", x); }
+
+template <typename... Args>
+void fold(Args... args) {
+  // unary right fold:  (args + ...)        -> (1 + (2 + (3 + 4)))
+  // unary left fold:   (... + args)        -> (((1 + 2) + 3) + 4)
+  // binary right fold: (args + ... + init) -> (1 + (2 + (3 + (4 + 100))))
+  // binary left fold:  (init + ... + args) -> ((((100 + 1) + 2) + 3) + 4)
+
+  // (std::cout << ... << args); // ((((std::cout << 1) << 2) << 3) << 4);
+  (log(args), ...);  // (log(1), (log(2), (log(3), (log(4)))))
+}
+
+template <typename... Args>
+void fillArray(std::vector<int>& arr, Args... args) {
+  (..., arr.push_back(args));
+  (arr.push_back(args), ...);
+}
+
+void printArray(const std::vector<int>& arr) {
+  for (const auto num : arr) util::debug("{} ", num);
+  util::debug("\n");
+}
+
+void sample() {
+  // fold(1, 2, 3, 4);
+
+  std::vector<int> arr;
+  fillArray(arr, 1, 2, 3, 4);
+  printArray(arr);
+}
+
+}  // namespace fold
+
+#endif  // CPP_FEATURES_FOLD_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,34 @@
 ﻿#define DEBUG
 
+#include <format>
+
 #include "util/log.h"
-#include "cpp_features/structured_binding.h"
+#include "cpp_features/fold.h"
+
+struct A {
+  int data;
+  A(int i) : data(i) { util::debug("constructor {}\n", data); }
+
+  A(const A& other) {
+    data = other.data;
+    util::debug("copy constructor {}\n", data);
+  }
+
+  A(const A&& other) noexcept {
+    data = other.data;
+    util::debug("move constructor {}\n", data);
+  }
+
+  ~A() { util::debug("destructor {}\n", data); }
+};
+
+A getA() {
+  A a1{1};
+  return (A{2}, A{3});
+}
+
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char argv[]) {
-  struct_binding::sample();
+  fold::sample();
   return 0;
 }
